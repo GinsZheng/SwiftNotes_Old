266ba7bb30ee4222f63994daf66dfeb14b5e8096
Gins Zheng <ginszheng@163.com>
2020-03-18 00:25:02 +0800

    a9.1

---
+++ SwiftNotes/Model/ModelNotes/ModelBasicNote.swift
@@ -0,0 +1,13 @@
+//
+//  ModelBasicNote.swift
+//  SwiftNotes
+//
+//  Created by GinsMac on 2020/3/18.
+//  Copyright © 2020 GinsMac. All rights reserved.
+//
+
+/*
+ 数据编写次序
+ ‣ 先把各个表的增删改查功能写出来
+ ‣ 再考虑联表查询
+ */
