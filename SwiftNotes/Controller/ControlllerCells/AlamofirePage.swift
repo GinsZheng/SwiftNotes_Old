96615e93c3c2982abb36033258094150dd1d79b7
Gins Zheng <ginszheng@163.com>
2022-10-05 12:02:26 +0800

    a89.2

--- SwiftNotes/Controller/ControlllerCells/AlamofirePage.swift
+++ SwiftNotes/Controller/ControlllerCells/AlamofirePage.swift
@@ -58,6 +58,25 @@ class CSAlamofirePage: UIViewController {
 
         
         // MARK: - 上传文件(如上传图片)
+        print("上传图片")
+        
+        // Bundle.main.url 可查到可以在工程文件->Build Phases->Copy Bundle Resource中看到
+        let fileURL = Bundle.main.url(forResource: "StarryNight", withExtension: "jpg")!
+        
+        AF.upload(multipartFormData: { (mutidata) in
+            mutidata.append(fileURL, withName: "file")
+        },
+                  to: "http://127.0.0.1:5000/upload_image").responseJSON { (response) in
+            if let value = response.result.value {
+                let json = JSON(value)
+                // print("json", json)
+                let image_url = json["image_url"].string ?? "(空)"
+                print("post 返回结果", "image_url =", image_url)
+            }
+        }
+        
+        
+        
 
             
     }
