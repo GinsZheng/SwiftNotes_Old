1c0768af24ad14fb1db95e7036c5564064c9e1bf
Gins Zheng <ginszheng@163.com>
2023-08-24 23:38:18 +0800

    b7

--- SwiftNotes/TestVC.swift
+++ SwiftNotes/TestVC.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
         tableView.register(DefaultTableViewCell.self, forCellReuseIdentifier: "DefaultTableViewCell")
         tableView.set(superview: view, delegate: self, dataSource: self, viewController: self)
-        tableView.setFrame(left: 0, top: 0, right: 0, height: kWithoutNavAndTabBarHeight)
+        tableView.setFrame(left: 0, top: 0, right: 0, height: kWithoutNavBarHeight)
 
     }
     
@@ -46,34 +46,4 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
 }
 
-
-class DefaultTableViewCell: UITableViewCell {
-    
-    let titleLabel = UILabel()
-    let nextIcon = UIImageView()
-    let separator = UIView()
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        titleLabel.set(superview: contentView)
-        titleLabel.setStyle17pt222()
-        titleLabel.setFrame(left: 16, centerY: contentView.centerY)
-        
-        nextIcon.set(superview: contentView, imageName: "next")
-        nextIcon.setFrame(right: 16, centerY: contentView.centerY, width: 18, height: 18)
-
-        separator.set(superview: contentView, backgroundColor: cSeparator)
-        separator.setSeparatorFrame(left: 16, right: 16)
-    }
-    
-}
 
