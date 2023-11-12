92df7a1bb8a92d514c1809df3ee5df3746b4e471
Gins Zheng <ginszheng@163.com>
2023-11-12 23:14:34 +0800

    b32

---
+++ SwiftNotes/Base/CollectionViewCell.swift
@@ -0,0 +1,105 @@
+//
+//  CollectionViewCell.swift
+//  SwiftNotes
+//
+//  Created by GinsMac on 2023/11/12.
+//  Copyright © 2023 GinsMac. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - 自定义 Cell
+
+class AutoLayoutCollectionViewCell2: UICollectionViewCell {
+    
+    // 输出参数(用于代理传递信息)
+    let fontsize: CGFloat = 17
+    let weight: UIFont.Weight = .medium
+    
+    weak var delegate: AutoLayoutCollectionViewCellDelegate?
+    
+    var titleOffset: CGFloat {
+        return delegate?.fetchTitleOffset() ?? 0
+    }
+    
+    let scrollView = UIScrollView()
+    let button = UIButton(type: .custom)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - func
+    
+    func setData(title: String) {
+        setBg()
+//        setTitle(text: title)
+        setButton(title: title)
+        resetWidth()
+    }
+    
+    func setBg() {
+//        scrollView.set(superview: contentView, backgroundColor: cF0F1F3)
+//        scrollView.setFrame(left: 0, top: 0, width: kScreenWidth, height: 200)
+    }
+    
+    func setButton(title: String) {
+        button.set(superview: contentView, target: target, action: #selector(pushToTest))
+        button.setStyleSolidButton(title: title, titleSize: 14, titleColor: c666, bgImage: getImageWithColor(color: cF0F1F3), radius: 14)
+        button.setFrame(left: 10, top: 10, width: 10, height: 28)
+    }
+    
+    func resetWidth() {
+        button.width = (button.titleLabel?.getLabelWidth() ?? 10) + titleOffset
+    }
+    
+//    func setTitle(text: String) {
+//        titleLabel.set(superview: imageView, text: text)
+//        titleLabel.setFontStyle(size: fontsize, color: cFFF, weight: weight, alignment: .center)
+//        titleLabel.setFrame(left: 10, centerY: imageView.centerY, width: titleLabel.getLabelWidth(), height: 20)
+//    }
+    
+//    func resetImageWidth() {
+//        imageView.width = titleLabel.getLabelWidth() + titleOffset
+//    }
+    
+    // MARK: - @objc func
+    @objc func pushToTest() {
+        print("done")
+    }
+    
+    
+}
+
+//
+//var frameRight: CGFloat = 0
+//
+//for (i, title) in buttonTitles.enumerated() {
+//    let button = UIButton(type: .custom)
+//    button.set(superview: scrollView, target: target, action: #selector(pushToTest))
+//    button.setTitle(title, for: .normal)
+//    button.setTitleColor(.hex(c666), for: .normal)
+//    button.titleLabel?.font = .systemFont(ofSize: 14, weight: .medium)
+//    button.setBackgroundColor(color: cF0F1F3)
+//    button.setCornerRadiusWithMask(radius: 14)
+//    button.setBackgroundImage(getImageWithColor(color: cDDDEE0), for: .highlighted)
+//    
+//
+//    let labelWidth = button.titleLabel?.getLabelWidth() ?? 0
+//    if i == 0 {
+//        button.setFrame(left: 10, centerY: scrollView.centerY, width: labelWidth + 24, height: 28)
+//    } else {
+//        button.setFrame(left: frameRight, centerY: scrollView.centerY, width: labelWidth + 24, height: 28)
+//    }
+//    
+//    frameRight = button.right + 6
+//    
+//    buttons.append(button)
+//}
+//
+//scrollView.contentSize = CGSize(width: frameRight + 4, height: 48)
--- SwiftNotes/TestVC.swift
+++ SwiftNotes/TestVC.swift
@@ -1,8 +1,28 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, AutoLayoutCollectionViewLayoutDelegate, AutoLayoutCollectionViewCellDelegate {
 
     var currentUIForm: UIForm = .form0
+    
+    // 输出参数
+    let titleOffset: CGFloat = 24
+    let itemInterval: CGFloat = 3
+    let itemHeight: CGFloat = 40
+    
+    var collectionView: UICollectionView!
+    
+    let dateSource = [
+        ["title":"0 Swift","bgColor":cBlue_5393FF],
+        ["title":"1 Xcode","bgColor":cPurple_BF62F8],
+        ["title":"2 Java","bgColor":cMagenta_FC5AAE],
+        ["title":"3 PHP","bgColor":cRed_FF635A],
+        ["title":"4 JS","bgColor":cOrange_F9AD18],
+        ["title":"5 Reac","bgColor":cGreen_25BE3C],
+        ["title":"6 Ruby","bgColor":cBluishGreen_01C7BD],
+        ["title":"7 HTML","bgColor":cBlue_5393FF],
+        ["title":"8 C# ","bgColor":cPurple_BF62F8],
+        ["title":"9 C++","bgColor":cPurple_BF62F8],
+    ]
     
     // MARK: - 生命周期方法
     
@@ -47,6 +67,9 @@ class ViewController: UIViewController {
         bgView.setFrame(allEdges: 0)
         bgView.setCornerRadius(radius: 10)
         
+        let buttonList = CSHorizonalScrollingButtonList(target: self, frame: CGRect(x: 0, y: 0, width: kScreenWidth, height: 48))
+        buttonList.set(superview: bgView)
+        
         let switchButton = UIButton(type: .custom)
         switchButton.set(superview: bgView, target: self, action: #selector(switchView))
         switchButton.setStyleIconButton(imageName: "groupBar_fold")
@@ -64,8 +87,15 @@ class ViewController: UIViewController {
         let bgView = UIView()
         bgView.set(superview: contentView, backgroundColor: cFFF)
         bgView.setFrame(allEdges: 0)
-        bgView.setCornerRadius(radius: 10)
-
+        bgView.setCornerRadiusWithMask(radius: 10)
+        
+        let frame = CGRect(x: 0, y: 0, width: kScreenWidth - 40, height: kWithoutNavBarHeight)
+        let layout = AutoLayoutCollectionViewLayout(titleOffset: titleOffset, itemInterval: itemInterval, itemHeight: itemHeight)
+        layout.delegate = self
+        collectionView = UICollectionView(frame: frame, collectionViewLayout: layout)
+        collectionView.register(AutoLayoutCollectionViewCell2.self, forCellWithReuseIdentifier: String(describing: AutoLayoutCollectionViewCell2.self))
+        collectionView.set(superview: bgView, delegate: self, dataSource: self, viewController: self)
+        
         let switchButton = UIButton(type: .custom)
         switchButton.set(superview: bgView, target: self, action: #selector(switchView))
         switchButton.setStyleIconButton(imageName: "groupBar_unfold")
@@ -74,6 +104,49 @@ class ViewController: UIViewController {
     
     
     // MARK: - 代理方法
+    // MARK: - CollectionView 代理方法
+    
+    // 设置数量
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dateSource.count
+    }
+    
+    // 设置点击事件
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.push(toTarget: CSGeneralSubpage())
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+    
+    // 设置 cell 逻辑
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: AutoLayoutCollectionViewCell2.self), for: indexPath) as! AutoLayoutCollectionViewCell2
+        cell.delegate = self
+        // 把UI逻辑放在自定义的 CollectionViewCell，把数据放在此
+        let data = dateSource[indexPath.row]
+        cell.setData(title: data["title"] ?? "")
+        
+        return cell
+    }
+    
+    
+    // MARK: - AutoLayoutCollectionViewLayoutDelegate 代理方法
+    
+    func fetchTitleWidths() -> [CGFloat] {
+        var titleWidths: [CGFloat] = []
+        for i in 0..<dateSource.count {
+            // ⚠️这里fontSize和weight要和下面的AutoLayoutCollectionViewCell2中的保持一致
+            let width = getLabelWidth(text: dateSource[i]["title"] ?? "", fontSize: 17, weight: .medium)
+            titleWidths.append(width)
+        }
+        
+        return titleWidths
+    }
+    
+    
+    // MARK: - AutoLayoutCollectionViewCellDelegate 代理方法
+    func fetchTitleOffset() -> CGFloat {
+        return titleOffset
+    }
     
     
     // MARK: - @objc func
