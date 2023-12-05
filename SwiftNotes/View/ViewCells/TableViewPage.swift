86a20b23b5cc87fdcd1f22c2813c51c346f89d3d
Gins Zheng <ginszheng@163.com>
2023-12-05 23:07:36 +0800

    b75

--- SwiftNotes/View/ViewCells/TableViewPage.swift
+++ SwiftNotes/View/ViewCells/TableViewPage.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TableViewPage: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TableViewPage: UIViewController {
     
     let list: [(String, UIViewController)] = [
         ("Animation", CSGeneralSubpage()),
@@ -19,29 +19,27 @@ class TableViewPage: UIViewController, UITableViewDelegate, UITableViewDataSourc
     
     
     // MARK: - 生命周期方法
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
     }
     
     
     // MARK: - func
-    
     func setupUI() {
         tableView.register(DefaultTableViewCell.self, forCellReuseIdentifier: String(describing: DefaultTableViewCell.self))
         tableView.set(superview: view, delegate: self, dataSource: self, viewController: self)
         tableView.setFrame(left: 0, top: 0, right: 0, height: kWithoutNavBarHeight)
     }
     
+    // MARK: - @objc func
     
-    // MARK: - tableview 代理方法
     
-    // 行数
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
-    }
+}
+
+
+// MARK: - 代理方法：UITableViewDelegate
+extension TableViewPage: UITableViewDelegate {
     
     // 行高
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -53,21 +51,24 @@ class TableViewPage: UIViewController, UITableViewDelegate, UITableViewDataSourc
         self.push(toTarget: CSGeneralSubpage())
         tableView.deselectRow(at: indexPath, animated: true)
     }
+}
+
+
+// MARK: - 代理方法：UITableViewDataSource
+extension TableViewPage: UITableViewDataSource {
+    
+    // 行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
     
     // cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: DefaultTableViewCell.self), for: indexPath) as! DefaultTableViewCell
         cell.titleLabel.setText(text: list[indexPath.row].0)
-        
         return cell
     }
-    
-    
-    // MARK: - @objc func
-    
-    
 }
-
 
 
 // MARK: - 自定义 tableViewCell
