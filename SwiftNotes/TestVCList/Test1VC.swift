fc6bbf3e3dc6e45bf8082d4d3fd08db36b5fd345
Gins Zheng <ginszheng@163.com>
2023-12-06 15:28:34 +0800

    b79

--- SwiftNotes/TestVCList/Test1VC.swift
+++ SwiftNotes/TestVCList/Test1VC.swift
@@ -5,37 +5,123 @@
 //  Created by GinsMac on 2023/11/24.
 //  Copyright © 2023 GinsMac. All rights reserved.
 //
-
-/// 本页为使用闭包在ViewController和CustomView之前传递数据的示例
-/// 视图控制器 (ViewController): 主要的控制器，它包含一个自定义视图
-/// 自定义视图 (CustomView): 一个简单的视图，它有一个方法updateData，用于更新其显示的数据。
-/// 数据传递：CustomView使用闭包从ViewController获取数据。
+//  Test1VC
 
 import UIKit
 
 class Test1VC: UIViewController {
     
-    // MARK: - 生命周期方法
+    let dataSource: [DefaultTableViewItem] = [
+        DefaultTableViewItem(title: "Animation", viewController: CSGeneralSubpage()),
+        DefaultTableViewItem(title: "Button", viewController: CSGeneralSubpage()),
+    ]
     
+    let tableView = UITableView()
+    
+    
+    // MARK: - 生命周期方法
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
     }
     
     
     // MARK: - func
-    
     func setupUI() {
-        view.setBackgroundColor(color: cFFF)
+        tableView.register(DefaultTableViewCell.self, forCellReuseIdentifier: DefaultTableViewCell.identifier)
+        tableView.set(superview: view, delegate: self, dataSource: self, viewController: self)
+        tableView.setFrame(left: 0, top: 0, right: 0, height: kWithoutNavBarHeight)
+    }
+    
+    
+    // MARK: - @objc func
+    
+    
+}
+
+
+// MARK: - 代理方法：UITableViewDelegate
+extension TableViewPage: UITableViewDelegate {
+    // 行高
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return kCellHeight
+    }
+    
+    // 点击
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.push(toTarget: dataSource[indexPath.row].viewController)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+
+// MARK: - 代理方法：UITableViewDataSource
+extension TableViewPage: UITableViewDataSource {
+    // 行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    // cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: DefaultTableViewCell.identifier, for: indexPath) as? DefaultTableViewCell else { return UITableViewCell() }
+        cell.configure(title: dataSource[indexPath.row].title)
+        return cell
+    }
+}
+
+
+// MARK: - TableView 默认数据结构体
+struct DefaultTableViewItem {
+    let title: String
+    let viewController: UIViewController
+}
+
+
+// MARK: - 自定义的默认 tableViewCell
+class DefaultTableViewCell: UITableViewCell {
+    
+    static let identifier = String(describing: DefaultTableViewCell.self)
+    
+    private let titleLabel = UILabel()
+    private let nextIcon = UIImageView()
+    private let separator = UIView()
+    
+    
+    // MARK: - 初始化
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - func
+    // 设置控件非布局内容 (严谨说是一次性设置的内容，这通常都是非布局内容)
+    private func setupUI() {
+        titleLabel.set(superview: contentView)
+        titleLabel.setStyle17pt222()
         
-        let customView = ClosureCustomView(frame: CGRect(x: 50, y: 100, width: 200, height: 50))
-        view.addSubview(customView)
+        nextIcon.set(superview: contentView, imageName: "next")
         
-        customView.fetchDataClosure = {
-            return "Hello from ViewController"
-        }
+        separator.set(superview: contentView, backgroundColor: cSeparator)
         
+    }
+    
+    // 设置控件布局 (严谨说是布局刷新时需要刷新的内容，这通常都是布局内容)
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel.setFrame(left: 16, centerY: contentView.centerY)
+        nextIcon.setFrame(right: 16, centerY: contentView.centerY, width: 18, height: 18)
+        separator.setSeparatorFrame(left: 16, right: 16)
+    }
+    
+    // 配置数据
+    func configure(title: String) {
+        titleLabel.text = title
     }
     
 }
