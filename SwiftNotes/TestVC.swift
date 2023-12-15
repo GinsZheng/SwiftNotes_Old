5a7024eaa217578b7614191a711c65c58c378c1e
Gins Zheng <ginszheng@163.com>
2023-12-15 17:18:48 +0800

    c19

--- SwiftNotes/TestVC.swift
+++ SwiftNotes/TestVC.swift
@@ -2,27 +2,127 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let dimmedBackgroundView = UIView()
+    let foregroundView = UIView()
+    let textField = UITextField()
+    let overlayButton = UIButton()
+    var foregroundViewBottomConstraint: NSLayoutConstraint?
     
-    
-    
-    // MARK: - 生命周期方法
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
+        setupViews()
+        setupConstraints()
+        setupNotifications()
     }
     
-    
-    // MARK: - func
-    func setupUI() {
-        view.setBackgroundColor(color: cFFF)
+    func setupViews() {
+        view.backgroundColor = .white
         
+        // 配置触发按钮
+        overlayButton.setTitle("显示叠加层", for: .normal)
+        overlayButton.backgroundColor = .blue
+        view.addSubview(overlayButton)
         
+        // 配置前景视图
+        foregroundView.backgroundColor = .white
+        view.addSubview(foregroundView)
+        
+        // 配置输入框
+        textField.borderStyle = .roundedRect
+        foregroundView.addSubview(textField)
+        
+        // 配置背景遮罩视图
+        dimmedBackgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        dimmedBackgroundView.alpha = 0
+        view.addSubview(dimmedBackgroundView)
     }
     
+    func setupConstraints() {
+        overlayButton.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        foregroundView.translatesAutoresizingMaskIntoConstraints = false
+        dimmedBackgroundView.translatesAutoresizingMaskIntoConstraints = false
         
-    // MARK: - @objc func
+        // 触发按钮的约束
+        overlayButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        overlayButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        overlayButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        overlayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        // 前景视图的约束
+        foregroundViewBottomConstraint = foregroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        NSLayoutConstraint.activate([
+            foregroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            foregroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            foregroundViewBottomConstraint!,
+            foregroundView.heightAnchor.constraint(equalToConstant: 300) // 假设前景层的高度是300
+        ])
+        
+        // 输入框的约束
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: foregroundView.topAnchor, constant: 20),
+            textField.leadingAnchor.constraint(equalTo: foregroundView.leadingAnchor, constant: 20),
+            textField.trailingAnchor.constraint(equalTo: foregroundView.trailingAnchor, constant: -20),
+            textField.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        // 背景遮罩视图的约束
+        NSLayoutConstraint.activate([
+            dimmedBackgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            dimmedBackgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            dimmedBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            dimmedBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
     
+    func setupNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
     
+    @objc func showOverlayView() {
+        dimmedBackgroundView.alpha = 0
+        foregroundView.alpha = 0
+        foregroundViewBottomConstraint?.constant = 0
+        
+        // 按钮点击事件
+        overlayButton.addTarget(self, action: #selector(toggleOverlay), for: .touchUpInside)
+        
+        UIView.animate(withDuration: 0.3) {
+            self.dimmedBackgroundView.alpha = 1
+            self.foregroundView.alpha = 1
+        }
+        
+        textField.becomeFirstResponder()
+    }
+    
+    @objc func toggleOverlay() {
+        textField.becomeFirstResponder()
+        showOverlayView()
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            foregroundViewBottomConstraint?.constant = -keyboardHeight
+            
+            UIView.animate(withDuration: 0.3) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        foregroundViewBottomConstraint?.constant = 0
+        
+        UIView.animate(withDuration: 0.3) {
+            self.dimmedBackgroundView.alpha = 0
+            self.foregroundView.alpha = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 }
-
-
---
+++ SwiftNotes/TestVCList/Test4VC.swift
@@ -0,0 +1,55 @@
+//临时，用于创建任务时的界面设置
+
+//import Foundation
+//import UIKit
+//
+//class ViewController: UIViewController {
+//    
+//    let dimmedBackgroundView = UIView()
+//    let customView = UIView() // 这将是你的自定义视图
+//    let textField = UITextField() // 输入框
+//    let overlayButton = UIButton() // 这是触发叠加视图的按钮
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        // 配置按钮
+//        overlayButton.setTitle("显示叠加层", for: .normal)
+//        overlayButton.backgroundColor = .blue
+//        overlayButton.frame = CGRect(x: 50, y: 100, width: 200, height: 50) // 这里设置按钮的位置和大小
+//        overlayButton.addTarget(self, action: #selector(showOverlayView), for: .touchUpInside)
+//        self.view.addSubview(overlayButton)
+//        
+//        // 配置背景视图
+//        dimmedBackgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+//        dimmedBackgroundView.frame = self.view.bounds
+//        dimmedBackgroundView.alpha = 0
+//        // 不直接将背景视图添加到self.view中，将在showOverlayView中添加
+//    }
+//    
+//    @objc func showOverlayView() {
+//        self.view.addSubview(dimmedBackgroundView)
+//        self.view.addSubview(customView)
+//        
+//        // 配置自定义视图的其他UI组件
+//        customView.backgroundColor = .white
+//        customView.frame = CGRect(x: 20, y: 200, width: self.view.frame.width - 40, height: 300) // 设置自定义视图的位置和大小
+//        customView.layer.cornerRadius = 10
+//        
+//        // 添加输入框
+//        textField.frame = CGRect(x: 10, y: 10, width: customView.frame.width - 20, height: 40)
+//        textField.borderStyle = .roundedRect
+//        customView.addSubview(textField)
+//        
+//        // 渐显背景视图和自定义视图
+//        UIView.animate(withDuration: 0.3) {
+//            self.dimmedBackgroundView.alpha = 1
+//            self.customView.alpha = 1
+//        }
+//        
+//        // 自动激活输入框
+//        textField.becomeFirstResponder()
+//    }
+//    
+//    // ... 其他代码，包括键盘的通知监听方法
+//}
