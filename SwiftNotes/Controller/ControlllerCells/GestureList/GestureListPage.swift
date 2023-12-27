78e1e693163ca40fa687edbe4429516732ae1cda
Gins Zheng <ginszheng@163.com>
2023-12-27 10:45:05 +0800

    c81

--- SwiftNotes/Controller/ControlllerCells/GestureList/GestureListPage.swift
+++ SwiftNotes/Controller/ControlllerCells/GestureList/GestureListPage.swift
@@ -8,59 +8,44 @@
 
 import UIKit
 
-class CSGestureListPage: UIViewController, UITableViewDelegate, UITableViewDataSource {
+private class DataManager: DefaultCellDataManager {
+    init() {
+        super.init(initialItems: [
+            .titleNextVC(title: "Disable Pop", viewController: CSDisablePopGesturePage()),
+            .titleNextVC(title: "Long Press", viewController: CSLongPressGesturePage()),
+            .titleNextVC(title: "Pan", viewController: CSPanGesturePage()),
+            .titleNextVC(title: "Pinch", viewController: CSPinchGesturePage()),
+            .titleNextVC(title: "Swipe", viewController: CSSwipeGesturePage()),
+            .titleNextVC(title: "Tap", viewController: CSTapGesturePage()),
+            .titleNextVC(title: "TouchesBegan", viewController: CSTouchesBeganGesturePage()),
+            .titleNextVC(title: "TouchesMoved", viewController: CSTouchesMovedGesturePage())
+        ])
+    }
+}
+
+
+class CSGestureListPage: UIViewController {
     
-    let tableData: [TempDefaultCellItem] = [
-        TempDefaultCellItem(title: "Disable Pop", viewController: CSDisablePopGesturePage()),
-        TempDefaultCellItem(title: "Long Press", viewController: CSLongPressGesturePage()),
-        TempDefaultCellItem(title: "Pan", viewController: CSPanGesturePage()),
-        TempDefaultCellItem(title: "Pinch", viewController: CSPinchGesturePage()),
-        TempDefaultCellItem(title: "Swipe", viewController: CSSwipeGesturePage()),
-        TempDefaultCellItem(title: "Tap", viewController: CSTapGesturePage()),
-        TempDefaultCellItem(title: "TouchesBegan", viewController: CSTouchesBeganGesturePage()),
-        TempDefaultCellItem(title: "TouchesMoved", viewController: CSTouchesMovedGesturePage())
-    ]
-        
+    private let tableData = DataManager()
+    
     let tableView = UITableView()
     
     
     // MARK: - 生命周期方法
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
     }
     
     
     // MARK: - func
     func setupUI() {
-        tableView.register(DefaultCell.self, forCellReuseIdentifier: String(describing: DefaultCell.self))
-        tableView.setup(superview: view, delegate: self, dataSource: self, viewController: self)
-        tableView.setFrame(left: 0, top: 0, right: 0, height: kWithoutNavBarHeight)
-    }
-    
-    
-    // MARK: - tableview 代理方法
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tableData.count
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return kCellHeight
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.push(toTarget: tableData[indexPath.row].viewController)
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: DefaultCell.self), for: indexPath) as! DefaultCell
-        cell.prepare(row: indexPath.row, dataCount: tableData.count)
-        cell.configure(cellType: .titleRightIcon, title: tableData[indexPath.row].title)
-        return cell
+        view.setBackgroundColor(color: cF2F3F6)
+        setupDefaultTableView(tableView)
+        // 数据更新时刷新列表
+        tableData.onItemsUpdated = { [weak self] in
+            self?.tableView.reloadData()
+        }
     }
     
     
@@ -69,54 +54,83 @@ class CSGestureListPage: UIViewController, UITableViewDelegate, UITableViewDataS
 }
 
 
-//import UIKit
-//
+// MARK: - TableView 代理方法
+extension CSGestureListPage: UITableViewDelegate, UITableViewDataSource {
+    // 行高
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return kCellHeight
+    }
+    
+    // 点击
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = tableData[indexPath.row]
+        item.handleCellTap(in: self)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // 行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    // cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: DefaultCell.identifier, for: indexPath) as? DefaultCell else { return UITableViewCell() }
+        cell.prepare(row: indexPath.row, dataCount: tableData.count)
+        let item = tableData[indexPath.row]
+        item.configureCell(cell)
+        return cell
+    }
+}
+
+
 //class CSGestureListPage: UIViewController, UITableViewDelegate, UITableViewDataSource {
-//
-//    let titleList = ["Disable Pop", "Long Press", "Pan", "Pinch", "Swipe", "Tap", "TouchesBegan", "TouchesMoved"]
-//    let pageList = [CSDisablePopGesturePage(), CSLongPressGesturePage(), CSPanGesturePage(), CSPinchGesturePage(), CSSwipeGesturePage(), CSTapGesturePage(), CSTouchesBeganGesturePage(), CSTouchesMovedGesturePage()]
-//    var cardHeight: CGFloat = 0
-//
+//    
+//    private let tableData = DataManager()
+//        
 //    let tableView = UITableView()
-//
+//    
+//    
+//    // MARK: - 生命周期方法
 //
 //    override func viewDidLoad() {
 //        super.viewDidLoad()
-//        view.setBackgroundColor(color: cFFF)
-//
-//        self.tableView.setup(superview: view, delegate: self, dataSource: self, viewController: self)
-//        tableView.setFrame(left: 0, top: 0, right: 0, bottom: 0)
+//        
+//        setupUI()
 //    }
-//
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = UITableViewCell(style: .value1, reuseIdentifier: "cellID1")
-//        cell.setFrame(left: 0, top: 0, width: kScreenWidth, height: kCellHeight)
-//        cell.setSeparator(left: 20, right: 0)
-//
-//        let cellTitle = UILabel()
-//        cellTitle.setup(superview: cell, text: titleList[indexPath.row])
-//        cellTitle.setFrame(left: 20, centerY: cell.centerY)
-//
-//        let next = UIImageView()
-//        next.setup(superview: cell, imageName: kIconNext)
-//        next.setFrame(right: 20, centerY: cell.centerY, width: 16, height: 16)
-//
-//        return cell
+//    
+//    
+//    // MARK: - func
+//    func setupUI() {
+//        tableView.register(DefaultCell.self, forCellReuseIdentifier: String(describing: DefaultCell.self))
+//        tableView.setup(superview: view, delegate: self, dataSource: self, viewController: self)
+//        tableView.setFrame(left: 0, top: 0, right: 0, height: kWithoutNavBarHeight)
 //    }
-//
+//    
+//    
+//    // MARK: - tableview 代理方法
 //
 //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return titledataSource.count
+//        return tableData.count
 //    }
-//
+//    
 //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 56
+//        return kCellHeight
 //    }
-//
+//    
 //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        self.push(toTarget: pageList[indexPath.row])
+//        self.push(toTarget: tableData[indexPath.row].viewController)
 //        tableView.deselectRow(at: indexPath, animated: true)
 //    }
-//
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: DefaultCell.self), for: indexPath) as! DefaultCell
+//        cell.prepare(row: indexPath.row, dataCount: tableData.count)
+//        cell.configure(cellType: .titleRightIcon, title: tableData[indexPath.row].title)
+//        return cell
+//    }
+//    
+//    
+//    // MARK: - @objc func
+//    
 //}
--- SwiftNotes/Model/ModelListVC.swift
+++ SwiftNotes/Model/ModelListVC.swift
@@ -53,11 +53,7 @@ class CSModelListVC: UIViewController {
     // MARK: - func
     func setupUI() {
         view.setBackgroundColor(color: cF2F3F6)
-        
-        tableView.register(DefaultCell.self, forCellReuseIdentifier: DefaultCell.identifier)
-        tableView.setup(superview: view, delegate: self, dataSource: self, viewController: self)
-        tableView.setFrame(left: 0, top: 0, right: 0, height: kWithoutNavBarHeight)
-        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: kVertMargin, right: 0)
+        setupDefaultTableView(tableView)
         // 数据更新时刷新列表
         tableData.onItemsUpdated = { [weak self] in
             self?.tableView.reloadData()
