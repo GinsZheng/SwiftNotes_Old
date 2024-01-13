afff6500dcc421eea7e04937a4c06f80bc00334a
Gins Zheng <ginszheng@163.com>
2024-01-13 23:05:38 +0800

    d72

--- SwiftNotes/TestVC/TestVC.swift
+++ SwiftNotes/TestVC/TestVC.swift
@@ -26,5 +26,5 @@ extension ViewController {
         }
         
     }
-
+    
 }
--- SwiftNotes/View/ViewCells/SwitchScrollAndCollectionView/SwitchScrollAndCollectionViewPage.swift
+++ SwiftNotes/View/ViewCells/SwitchScrollAndCollectionView/SwitchScrollAndCollectionViewPage.swift
@@ -17,18 +17,17 @@ class SwitchScrollAndCollectionViewPage: UIViewController {
         super.viewDidLoad()
         setupUI()
     }
+    
+}
 
-    // MARK: - func
-    func setupUI() {
+
+// MARK: - 私有方法
+extension SwitchScrollAndCollectionViewPage {
+    private func setupUI() {
         view.setBackgroundColor(color: cBgGray)
         groupListView = GroupListView(frame: .zero, viewController: self, showTrashButton: true)
         // 可以设置 groupListView 的其他属性或者约束
         groupListView.setup(superview: view)
         groupListView.setupView()
     }
-    
-    // MARK: - @objc func
-    
 }
-
-
