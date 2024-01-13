20d41c5d69d6b51aed5f3cdf7db53d44ca8bd2ee
Gins Zheng <ginszheng@163.com>
2024-01-13 18:25:25 +0800

    d69

--- SwiftNotes/TestVC/TestVC.swift
+++ SwiftNotes/TestVC/TestVC.swift
@@ -12,20 +12,15 @@ class ViewController: UIViewController {
     // MARK: - func
     func setupUI() {
         view.setBackgroundColor(color: cBgGray)
-        
-//        let titleView = DefaultViewOfCell()
-//        titleView.setup(superview: view)
-//        titleView.prepare(row: 0, cellCountInSection: 1)
-//        titleView.configure(cellType: .title, title: "时间")
-        
-        let titleVCView = DefaultViewOfCell()
+
+        let titleVCView = DefaultViewOfCell(cornerType: .allCorners)
         titleVCView.setup(superview: view)
         titleVCView.setFrame(left: 0, top: 100, right: 0, height: kCellHeight)
-        titleVCView.prepare(cornerType: .topCorners, row: 0, cellCountInSection: 1)
         titleVCView.configure(cellType: .titleRightIcon, title: "提醒")
+        titleVCView.onTap = {
+            self.push(targetVC: CSGeneralSubpage())
+        }
     }
-    
-    // MARK: - @objc func
     
     
 }
