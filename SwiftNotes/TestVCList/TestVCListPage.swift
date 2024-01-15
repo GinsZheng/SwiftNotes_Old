e81e6bcc2fb0d92f58d82f3b1a9c0033e3a6f3a8
Gins Zheng <ginszheng@163.com>
2024-01-15 10:44:24 +0800

    d77

--- SwiftNotes/TestVCList/TestVCListPage.swift
+++ SwiftNotes/TestVCList/TestVCListPage.swift
@@ -5,10 +5,10 @@ private class DataManager: DefaultCellDataManager {
         super.init(initialItems: [
             .titleNextVC(title: "Test1", viewController: Test1VC()),
             .titleNextVC(title: "Table View", viewController: TableViewPage()),
-            .titleNextVC(title: "Sections Table View", viewController: SectionsTableViewPage()),
-            .titleNextVC(title: "FoldableTableViewPage", viewController: FoldableTableViewPage()),
             .titleNextVC(title: "SectionsTableViewPage", viewController: SectionsTableViewPage()),
+            .titleNextVC(title: "MultiCellTypeTableViewPage", viewController: MultiCellTypeTableViewPage()),
             .titleNextVC(title: "MoreTableViewListPage", viewController: MoreTableViewListPage()),
+
         ])
     }
 }
