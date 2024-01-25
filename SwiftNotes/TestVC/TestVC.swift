8fd86cb9ec1ba2be617f033688139ee4e21106bb
Gins Zheng <ginszheng@163.com>
2024-01-25 17:52:17 +0800

    f42

--- SwiftNotes/TestVC/TestVC.swift
+++ SwiftNotes/TestVC/TestVC.swift
@@ -1,5 +1,29 @@
 import UIKit
 
+
+struct HomeCellItems {
+    var id: Int
+    var taskType: Int                       // 0：任务，1：子任务
+    var title: String
+    var isDone: Bool
+    var isReminded: Bool
+    var isTimeSet: Int?
+    var nextReminderTime: Int?
+    var isRepeating: Bool
+    var isProgressing: Bool
+    var color: Int                          // 0:蓝色 | 1:紫 | 2:品红 | 3:红 | 4:橙 | 5:绿
+    var priority: Int                       // 0:无 | 1:低 | 2:中 | 3:高
+    var subtaskIdList: [Int]?               // ⚠️可能应该把List转一种表达
+    var creationTime: Int
+    var updateTime: Int
+    var manualSorting: Int
+    var progressPercentage: Int?             // 还需要：进度值、
+}
+
+
+
+
+
 class ViewController: UIViewController {
     var itemTable = ItemTable()
     
@@ -24,3 +48,39 @@ extension ViewController {
     }
     
 }
+
+
+struct AllTaskItems {
+    var id: Int
+    var taskType: Int                       // 0：任务，1：子任务
+    var title: String
+    var description: String?
+    var isDone: Bool
+    var isReminded: Bool
+    var isTimeSet: Bool?
+    var nextReminderTime: Int?
+    var reminderOccasionsList: [Int]?       // 时间
+    var isRepeating: Bool
+    var repeatCycle: Int?                   // 0:天 | 1:周 | 2:月 | 3:年
+    var repeatInterval: Int?                // 如：每n天提醒一次，间隔就是这里的n
+    var weekList: [Int]?                    // 0:周日 | 1:周一 | …(周二~周五依次) | 6:周六 (7个枚举值)
+    var repeatType: Int?                    // 0:按日期 | 1:按星期
+    var repeatDateList: [Int]?              // 数据范围：1-31
+    var whichWeek: Int?                     // 0:第一个星期 | …(依次) | 4:第五个星期 | 5:最后一个星期
+    var dayOfWeek: Int?                     // 0:周日 | 1:周一 | …(周二~周五依次) | 6:周六 | 7:自然日 | 8:工作日 | 9:周末
+    var monthsList: Int?                    // 1:1月 | (2月-11月依次) | 12:12月
+    var endRepeatTime: Int?
+    var isProgressing: Bool
+    var progressType: Int?                  // 0:手动进度 | 1:时长进度 | 2:计时进度
+    var totalProgressValue: Int?
+    var color: Int                          // 0:蓝色 | 1:紫 | 2:品红 | 3:红 | 4:橙 | 5:绿
+    var priority: Int                       // 0:无 | 1:低 | 2:中 | 3:高
+    var subtaskIdList: [Int]?
+    var creationTime: Int
+    var updateTime: Int
+    var groupId: Int
+    var isProgressDescriptionHidden: Bool
+    var manualSorting: Int
+    var isInTrash: Bool
+}
+
