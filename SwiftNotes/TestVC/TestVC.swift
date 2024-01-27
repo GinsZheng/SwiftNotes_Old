1b4a6656b7ad81f7e73a06fa510444367c7bb40d
Gins Zheng <ginszheng@163.com>
2024-01-28 00:18:01 +0800

    f44

--- SwiftNotes/TestVC/TestVC.swift
+++ SwiftNotes/TestVC/TestVC.swift
@@ -8,14 +8,14 @@ struct HomeCellItems {
     var isDone: Bool
     var isReminded: Bool
     var isTimeSet: Int?
-    var nextReminderTime: Int?
+    var nextReminderTimestamp: Int?
     var isRepeating: Bool
-    var isProgressing: Bool
+    var hasProgress: Bool
     var color: Int                          // 0:蓝色 | 1:紫 | 2:品红 | 3:红 | 4:橙 | 5:绿
     var priority: Int                       // 0:无 | 1:低 | 2:中 | 3:高
     var subtaskIdList: [Int]?               // ⚠️可能应该把List转一种表达
-    var creationTime: Int
-    var updateTime: Int
+    var creationTimestamp: Int
+    var updateTimestamp: Int
     var manualSorting: Int
     var progressPercentage: Int?             // 还需要：进度值、
 }
@@ -55,7 +55,7 @@ struct AllTaskItems {
     var isDone: Bool
     var isReminded: Bool
     var isTimeSet: Bool?
-    var nextReminderTime: Int?
+    var nextReminderTimestamp: Int?
     var reminderOccasions: [Int]?           // 时间
     var isRepeating: Bool
     var repeatCycle: Int?                   // 0:天 | 1:周 | 2:月 | 3:年
@@ -66,15 +66,15 @@ struct AllTaskItems {
     var whichWeek: Int?                     // 0:第一个星期 | …(依次) | 4:第五个星期 | 5:最后一个星期
     var dayOfWeek: Int?                     // 0:周日 | 1:周一 | …(周二~周五依次) | 6:周六 | 7:自然日 | 8:工作日 | 9:周末
     var months: Int?                    // 1:1月 | (2月-11月依次) | 12:12月
-    var endRepeatTime: Int?
-    var isProgressing: Bool
+    var endRepeatTimestamp: Int?
+    var hasProgress: Bool
     var progressType: Int?                  // 0:手动进度 | 1:时长进度 | 2:计时进度
     var totalProgress: Int?
     var color: Int                          // 0:蓝色 | 1:紫 | 2:品红 | 3:红 | 4:橙 | 5:绿
     var priority: Int                       // 0:无 | 1:低 | 2:中 | 3:高
     var subtaskIds: [Int]?
-    var creationTime: Int
-    var updateTime: Int
+    var creationTimestamp: Int
+    var updateTimestamp: Int
     var groupId: Int
     var isProgressDescriptionHidden: Bool
     var manualSorting: Int
