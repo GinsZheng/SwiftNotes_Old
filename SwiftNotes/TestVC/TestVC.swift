e523167548095ac46c76da650a6e6e114ffe5794
Gins Zheng <ginszheng@163.com>
2024-01-30 11:31:14 +0800

    f52

--- SwiftNotes/TestVC/TestVC.swift
+++ SwiftNotes/TestVC/TestVC.swift
@@ -1,25 +1,24 @@
-//import UIKit
-//
-//class ViewController: UIViewController {
-//    var itemTable = ItemTable()
-//    
-//    // MARK: - 初始化与生命周期方法
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        setupUI()
-//    }
-//    
-//}
-//
-//
-//// MARK: - 私有方法
-//extension ViewController {
-//    private func setupUI() {
-//        view.setBackgroundColor(color: cBgGray)
-//
-//        
-//        
-//    }
-//    
-//}
-//
+import UIKit
+
+class ViewController: UIViewController {
+    var itemTable = ItemTable()
+    
+    // MARK: - 初始化与生命周期方法
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+}
+
+
+// MARK: - 私有方法
+extension ViewController {
+    private func setupUI() {
+        view.setBackgroundColor(color: cBgGray)
+
+        
+    }
+    
+}
+
